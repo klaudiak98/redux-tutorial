aac835f9ef4e3cfd71de2133f4ad74ab65b3451f
Klaudia Kieryk <klaudiakieryk123@gmail.com>
2023-12-23 15:25:01 +0100

    Manage store with Redux

--- src/store/index.js
+++ src/store/index.js
@@ -6,15 +6,35 @@ const songSlice = createSlice({
     reducers: {
         addSong(state, action) {
             state.push(action.payload)
+        },
+        removeSong(state, action) {
+            const index = state.indexOf(action.payload)
+            state.splice(index, 1)
+        }
+    }
+})
+
+const moviesSlice = createSlice({
+    name: 'movie',
+    initialState: [],
+    reducers: {
+        addMovie(state, action) {
+            state.push(action.payload)
+        },
+        removeMovie(state, action) {
+            const index = state.indexOf(action.payload)
+            state.splice(index, 1)
         }
     }
 })
 
 const store = configureStore({
     reducer: {
-        songs: songSlice.reducer
+        songs: songSlice.reducer,
+        movie: moviesSlice.reducer
     }
 })
 
 export { store };
-export const { addSong } = songSlice.actions;
+export const { addSong, removeSong } = songSlice.actions;
+export const { addMovie, removeMovie } = moviesSlice.actions;
